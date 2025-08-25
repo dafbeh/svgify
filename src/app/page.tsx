78525b118f5fb0ff0ec94310127777c134b96341
Daf <8thatfrog@gmail.com>
2025-08-25 21:12:46 +0100

    Frontend bottom sections
    - Customisation
    - Generated Code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Svgify from '../components/svgify';
 
 export default function Home() {
   return (
-    <div className="min-h-screen min-w-screen bg-white dark:bg-[#343434]">
+    <div className="min-h-screen min-w-screen bg-white dark:bg-[#2c2d30]">
       <div className="flex justify-end p-3">
         <ThemeToggle />
       </div>
--- src/components/svgify.tsx
+++ src/components/svgify.tsx
@@ -9,16 +9,19 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-import { Upload, FileImage, Eye, Layers } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Upload, FileImage, Eye, Layers, Palette, Code, Copy } from 'lucide-react';
 
 export default function Svgify() {
     return (
-        <div className="flex justify-center w-full font-sans">
-            <div className="flex flex-col md:w-1/2 md:p-0 p-5">
+        <div className="flex justify-center w-full font-sans pb-12">
+            <div className="flex flex-col xl:w-1/2 w-full xl:p-0 p-5">
                 <h1 className={`text-5xl font-bold`}>Svgify</h1>
                 <p className="pt-3">Upload, customize, and export SVG icons as React/Vue components or CSS backgrounds.</p>
-                <div className="flex grid grid-cols-2 gap-8 pt-8">
-                    <Card className="h-95">
+                <div className="flex grid md:grid-cols-2 grid-cols-1 gap-8 pt-8">
+                    {/* Upload SVG */}
+                    <Card className="h-95 text-center">
                         <CardHeader>
                             <CardTitle className="flex items-center pl-1">
                                 <Upload className="mr-2" strokeWidth={2} size="20" /> 
@@ -41,7 +44,8 @@ export default function Svgify() {
                         </CardContent>
                     </Card>
 
-                    <Card className="h-95">
+                    {/* Preview */}
+                    <Card className="h-95 text-center">
                         <CardHeader>
                             <CardTitle className="flex items-center pl-1">
                                 <Eye className="mr-2" strokeWidth={2} size="20" /> 
@@ -53,6 +57,66 @@ export default function Svgify() {
                                 flex flex-col justify-center items-center gap-4">
                                 <Layers strokeWidth={1.75} size="50" />
                                 <p className="mb-1">Upload SVG to see preview</p>
+                            </div>
+                        </CardContent>
+                    </Card>
+
+                    {/* Customisation */}
+                    <Card className="min-h-95">
+                        <CardHeader>
+                            <CardTitle className="flex items-center pl-1">
+                                <Palette className="mr-2" strokeWidth={2} size="20" /> 
+                                Customisation
+                            </CardTitle>
+                        </CardHeader>
+                        <CardContent>
+                            <div>
+                                <div className="flex grid grid-cols-2 gap-4 w-full">
+                                    <div className="flex flex-col">
+                                        <p>Fill Colour</p>
+                                        <div className="pt-2 flex items-center">
+                                          <input className="w-9 h-9"
+                                            type="color"
+                                            id="fill"
+                                            />
+                                            <Input className="mx-2 focus:outline-green-500" defaultValue="#000000" />
+                                        </div>
+                                    </div>
+
+                                    <div className="flex flex-col">
+                                        <p>Stroke Colour</p>
+                                        <div className="pt-2 flex items-center">
+                                          <input className="w-9 h-9"
+                                            type="color"
+                                            id="stroke"
+                                            />
+                                            <Input className="mx-2" defaultValue="#000000" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </CardContent>
+                    </Card>
+
+                    {/* Generated Code */}
+                    <Card className="min-h-95">
+                        <CardHeader>
+                            <CardTitle className="flex items-center pl-1">
+                                <Code className="mr-2" strokeWidth={2} size="20" /> 
+                                Generated Code
+                            </CardTitle>
+                        </CardHeader>
+                        <CardContent>
+                            <Button variant="outline" className="mb-3 cursor-pointer">
+                                <Copy /> Copy
+                            </Button>
+                            <div className="w-full min-h-68 rounded-md
+                                flex flex-col justify-center items-center gap-4">
+                                <textarea className="w-full min-h-68 p-2 focus:ring-2 text-sm font-mono
+                                    ring-green-500 rounded-md focus:outline-none duration-100 transition"
+                                    placeholder="Generated code will appear here..."
+                                    readOnly
+                                />
                             </div>
                         </CardContent>
                     </Card>
